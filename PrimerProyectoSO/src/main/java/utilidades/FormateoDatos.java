da91beb0a114145062688963f0a1986dc680e9cd
Marcos <sierramarcos146@gmail.com>
2020-02-25 23:48:07 -0600

    Creacion de Clases de Utilidades
    Se creo la clase EstadoProces(enumeracion), FormateoDatos que es una clase auxiliar para cualquier conversion necesarioa, Proceso clase que representa al objeto proceso con todos los metodos necesarios

---
+++ PrimerProyectoSO/src/main/java/utilidades/FormateoDatos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author marcos
+ */
+public class FormateoDatos {
+    public static final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd.MM.yyy");
+    public static final SimpleDateFormat FORMATO_HORA = new SimpleDateFormat("hh:mm:ss");
+}
---
+++ PrimerProyectoSO/src/main/java/utilidades/Proceso.java
@@ -0,0 +1,108 @@
+
+package utilidades;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Marcos Sierra 
+ */
+public class Proceso extends Thread{
+    private final String ID;
+    private final Date INICIO;
+    private Date fechaFinal;
+    private double total;
+    private double restante;
+    private String estado;
+    /**
+     * Constructor del nuevo proceso.
+     * @param id  Nombre que recibira el proceso.
+     * 
+     */
+    
+    public Proceso(String id){
+        this.INICIO = new Date();
+        this.ID = id;
+        fechaFinal = null;
+        this.total = -1;
+        Random nuevoAleatorio = new Random(System.currentTimeMillis());
+        while(total < 0){
+            total = nuevoAleatorio.nextInt(100);
+        }
+        this.restante = total;
+        this.estado = EstadoProceso.NUEVO.getEstado();
+    }
+    
+    @Override
+    public void run(){
+        while(!(this.estado.equals(EstadoProceso.FINALIZADO.getEstado()))){
+            if(this.estado.equals(EstadoProceso.ATENDIDO.getEstado())){
+                    this.restante -= .1;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Proceso.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        this.fechaFinal = new Date();
+    }
+    /**
+     * Cambia el estado a EN_ESPERA
+     * 
+     */
+    public void setEstadoEspera(){
+        this.estado = EstadoProceso.EN_ESPERA.getEstado();
+    }
+    /**
+     * Cambia el estado a ATENDIDO
+     */
+    public void setEstadoAtendido(){
+        this.estado = EstadoProceso.ATENDIDO.getEstado();
+    }
+    /**
+     * Cambia el estado a FINALIZADO
+     */
+    public void setEstadoFinalizado(){
+        this.estado = EstadoProceso.FINALIZADO.getEstado();
+    }
+    /**
+     * 
+     * @return Devuelve String con la Fecha en la que se creo el proceso 
+     */
+    public String getFechaInicio(){
+        return FormateoDatos.FORMATO_FECHA.format(INICIO);
+    }
+    /**
+     * 
+     * @return Devuelve String con la Hora a la que se creo el metodo
+     */
+    public String getHoraInicio(){
+        return FormateoDatos.FORMATO_HORA.format(INICIO);
+    }
+    /**
+     * 
+     * @return Devuelve String con la fecha en la que termino el proceso, en caso no haber terminado devuelve el estado 
+     */
+    public String getFechaFin(){
+        if(this.fechaFinal != null){
+            return FormateoDatos.FORMATO_FECHA.format(INICIO);
+        }else{
+            return this.estado;
+        }
+    }
+    /**
+     * 
+     * @return Devuelve String con la hora en la que termino el proceso, en caso no haber terminado devuelve el estado en el que se encuentra
+     */
+    public String getHoraFin(){
+        if(this.fechaFinal != null){
+            return FormateoDatos.FORMATO_HORA.format(INICIO);
+        }else{
+            return this.estado;
+        }
+    }
+    
+}
