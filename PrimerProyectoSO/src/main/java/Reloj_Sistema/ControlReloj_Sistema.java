d4b68c8d1e152edd6510458aae3729222355857a
Gilda <gmrp22@hotmail.com>
2020-03-05 15:04:10 -0600

    Creacion de clase de manejo de reloj del sistema

---
+++ PrimerProyectoSO/src/main/java/Reloj_Sistema/ControlReloj_Sistema.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reloj_Sistema;
+
+/**
+ *
+ * @author tito88
+ */
+public class ControlReloj_Sistema {
+    
+}
